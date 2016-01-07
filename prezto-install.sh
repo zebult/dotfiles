95b586a1e8d76d0dfce6418644e9d14297825082
zebra <cisa12076@g.nihon-u.ac.jp>
2016-01-07 19:12:06 +0900

    add prezto install shell

---
+++ prezto-install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+// zsh起動
+zsh
+
+// リポジトリをclone
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+// 設定ファイルを作成
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+// Shellのデフォルトに設定
+chsh -s /bin/zsh
+
+echo "🌈finish!!! try logout after in"
