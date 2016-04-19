1e3ee0db970bdb36587020638c823d29346c533a
saichi <suzuki_saichi@cyberagent.co.jp>
2016-04-19 16:53:26 +0900

    update zshrc

--- prezto-install.sh
+++ prezto-install.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-// zsh起動
+# zsh起動
 zsh
 
-// リポジトリをclone
+# リポジトリをclone
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-// 設定ファイルを作成
+# 設定ファイルを作成
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-  done
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
 
-  // Shellのデフォルトに設定
-  chsh -s /bin/zsh
+# Shellのデフォルトに設定
+chsh -s /bin/zsh
 
-  echo "finish!!! try logout after in"
+echo "finish!!! try logout after in"
