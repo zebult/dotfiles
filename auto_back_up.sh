9b1aab14793db612189e473bc7b3b942e878c50b
zebra31 <ss.03100310@gmail.com>
2016-05-07 09:28:26 +0900

    Auto update 2016_05_07_09

--- auto_back_up.sh
+++ auto_back_up.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-cd /0CheatSheet
+cd /0cheatseat
 git add .
-git commit -m "Auto update"`date +%Y_%m_%d_%H` 
+git commit -m "Auto update "`date +%Y_%m_%d_%H` 
 git pull origin master
 git push origin master
 
 cd ~/dotfiles
 git add .
-git commit -m "Auto update"`date +%Y_%m_%d_%H` 
+git commit -m "Auto update "`date +%Y_%m_%d_%H` 
 git pull origin master
 git push origin master
