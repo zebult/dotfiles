89da6e975f6011eb98a65e4d6b5ad9ec91bd0599
saichi <suzuki_saichi@cyberagent.co.jp>
2016-06-27 13:26:52 +0900

    data update

--- shell/tmuxInit.sh
+++ shell/tmuxInit.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
-# tmux new-window -n zsh # 新しいwindowを開きたくない場合は以下
-# tmux rezsh-window zsh
+# tmux new-window -n default # 新しいwindowを開きたくない場合は以下
+tmux rename-window default
 tmux split-window -h
-tmux split-window -v -t zsh.1
-tmux split-window -v -t zsh.2
+tmux split-window -v -t default.1
+tmux split-window -v -t default.2
 
-# tmux send-keys -t zsh.1 'curl wttr.in/Moon' C-m
-tmux send-keys -t zsh.2 'curl wttr.in/Moon' C-m
-# tmux send-keys -t zsh.3 'curl wttr.in/Tokyo' C-m
-tmux send-keys -t zsh.4 'curl wttr.in/Tokyo' C-m
-tmux select-pane -t zsh.1
+# tmux send-keys -t default.1 'curl wttr.in/Moon' C-m
+tmux send-keys -t default.2 'curl wttr.in/Moon' C-m
+# tmux send-keys -t default.3 'curl wttr.in/Tokyo' C-m
+tmux send-keys -t default.4 'curl wttr.in/Tokyo' C-m
+tmux select-pane -t default.1
